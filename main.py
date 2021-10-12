447c80e939d371415058b3750a0700ce8abb82e2
Jamie Cole <jamiejcole@icloud.com>
2021-10-12 12:25:42 +1100

    updated inital file to work with pygame

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# hello
+import sys
+import time
+import pygame
+
+pygame.init()
+
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("TicTacToe")
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+	pygame.display.update()
