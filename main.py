e993abc9c11cda8393d605990c4adaec8d2ba2d3
Jamie Cole <jamiejcole@icloud.com>
2021-10-13 13:07:36 +1100

    added fonts and webapp code

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import sys
 import time
-import pygame
+import pygame, pygame.freetype
 
 pygame.init()
 
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("TicTacToe")
+
+font = pygame.freetype.Font("opensans.ttf", 30)
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+
+	screen.fill(RED)
+	
+	textSurface = font.render("Hello there", (0, 0, 0))
+	screen.blit(textSurface, (0, 0))
+
+
 	pygame.display.update()
