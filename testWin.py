852ff2489f85ee6a73b1ff7c24a0320a9b4ee41a
Jamie Cole <jamiejcole@icloud.com>
2021-10-31 22:14:42 +1100

    toby magic touch

--- testWin.py
+++ testWin.py
@@ -1,22 +1,19 @@
 moves = [
-	'/', '/', '/',
-	'/', '/', '/',
-	'/', '/', '/'
+	'1', '1', '1',
+	'1', '/', '/',
+	'1', '/', '/'
 ]
 
 def detectWin():
-	#if moves[0] == moves[1] and moves [0] == moves[2]:
-		#return moves[0], 0, 2
-
 	for i in range(0, 8, 3):
 		#print(i)
-		if moves[i] == moves[i + 1] and moves [i] == moves[i + 2] and moves[i] != '/':
-			print(i, "yeeman")
+		if moves[i] == moves[i + 1] and moves[i] == moves[i + 2] and moves[i] != '/':
+			print(i, "horizon")
 
-# still need to do columns in the for loop
-# and diagonals
+	for i in range(0, 2):
+		if moves[i] == moves[i + 3] and moves[i] == moves[i + 6] and moves[i] != '/':
+			print(i, "colm")
 
-
-
+# still need to do diagonals
 
 detectWin()
